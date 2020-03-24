5111fed8d7c37dd708abfcd133dcbdc6ac99e2b0
alfonsodelavega <alfonso.delavega@tuta.io>
2020-03-24 15:49:14 +0000

    Add baseline implementation of ViewTree diffMerge
    Currently:
    - All nodes of the viewtree are included in the differences (easier
      to debug at first).
    - Each viewtree node's name is updated to reflect any change (e.g.
      new, modified, deleted, or same if no changes are present).
    - At the moment modified nodes just show a "Content differs" message
    
    Next:
    - Employ diffEngines to perform diffs in those nodes based on the
      format of the compared viewtrees (e.g. DotDiffEngine).
    - Develop the default diff engine for when no appropriate engine
      for the format is present. Based on the trello ticket, I'll
      include a SideBySideDiffEngine.

--- org.eclipse.epsilon.picto.diff/src/org/eclipse/epsilon/picto/diff/ViewTreeMerger.java
+++ org.eclipse.epsilon.picto.diff/src/org/eclipse/epsilon/picto/diff/ViewTreeMerger.java
@@ -1,37 +1,123 @@
 package org.eclipse.epsilon.picto.diff;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.epsilon.picto.StringContentPromise;
 import org.eclipse.epsilon.picto.ViewTree;
 
 public class ViewTreeMerger {
 
-//	public static ViewTree merge(ViewTree left, ViewTree right) {
-//		ViewTree result = new ViewTree("Model Diff View");
-//
-//		result = append(result, calculateDifference(left, right), "Differences");
-//		result = append(result, left, "Left Model");
-//		result = append(result, right, "Right Model");
-//
-//		return result;
-//	}
+	public static void main(String[] args) throws Exception {
 
-	public static void append(ViewTree left, ViewTree right, List<String> parentPath, String leafName) {
+		ViewTree pathTree = new ViewTree();
+		pathTree.addPath(Arrays.asList("e1", "e2"),
+				new StringContentPromise("c1"), "text", "", Collections.emptyList());
+		pathTree.addPath(Arrays.asList("e1", "e3", "e4"),
+				new StringContentPromise("c2"), "text", "", Collections.emptyList());
+		pathTree.addPath(Arrays.asList("e1", "e3", "e5"),
+				new StringContentPromise("This is a very very long content in a view tree"), "text", "",
+				Collections.emptyList());
+		System.out.println(pathTree);
 
+	}
+
+	public static ViewTree diffMerge(ViewTree left, ViewTree right) throws Exception {
+		return diffMerge(new ViewTree(), Arrays.asList(""), left, right);
+	}
+
+	private static ViewTree diffMerge(ViewTree mergedViewTree, List<String> currentPath,
+			ViewTree left, ViewTree right) throws Exception {
+
+		ViewTree currentLeft = left.forPath(currentPath);
+		ViewTree currentRight = right.forPath(currentPath);
+		List<ViewTree> remainingRightChildren = new ArrayList<>(currentRight.getChildren());
+
+		for (ViewTree leftChild : currentLeft.getChildren()) {
+			ViewTree counterpart = null;
+			ViewTree diff = null;
+			for (ViewTree rightChild : remainingRightChildren) {
+				if (leftChild.getName().equals(rightChild.getName())) {
+					counterpart = rightChild;
+					remainingRightChildren.remove(counterpart);
+					diff = copy(leftChild);
+					if (leftChild.getPromise() == null && counterpart.getPromise() == null) {
+						diff.setPromise(new StringContentPromise("Both viewtrees empty"));
+						diff.setFormat("text");
+					}
+					else if (leftChild.getPromise() == null || counterpart.getPromise() == null) {
+						diff.setPromise(new StringContentPromise("Some viewtree empty"));
+						diff.setFormat("text");
+					}
+					else if (leftChild.getPromise().getContent().equals(counterpart.getPromise().getContent())) {
+						diff.setName(String.format("%s (Same)", diff.getName()));
+					}
+					else {
+						diff.setName(String.format("%s (Modified)", diff.getName()));
+						diff.setPromise(new StringContentPromise(String.format("%s: Content differs", leftChild.getName())));
+						diff.setFormat("text");
+					}
+					if (!leftChild.getChildren().isEmpty()) {
+						List<String> newPath = new ArrayList<>(currentPath);
+						newPath.add(leftChild.getName());
+						diff = diffMerge(diff, newPath, left, right);
+					}
+					break;
+				}
+			}
+			if (counterpart == null) {
+				// deleted elements
+				diff = copy(leftChild);
+				diff.setName(String.format("%s (Deleted)", leftChild.getName()));
+			}
+			append(mergedViewTree, diff, "");
+		}
+
+		for (ViewTree remainingChild : remainingRightChildren) {
+			// new elements
+			ViewTree newChild = copy(remainingChild);
+			newChild.setName(String.format("%s (New)", remainingChild.getName()));
+			newChild.getChildren().addAll(remainingChild.getChildren());
+			append(mergedViewTree, newChild, "");
+		}
+
+		return mergedViewTree;
+	}
+
+	public static void append(ViewTree left, ViewTree right, String leafName) {
+
+		ViewTree child = null;
 		// don't create an extra children level if the appended viewtree is anonymous
-		ViewTree child = new ViewTree(leafName);
-		if (right.getName().equals("")) {
-			child.setPromise(right.getPromise());
-			child.setFormat(right.getFormat());
-			child.setIcon(right.getIcon());
+		// or if the provided leaf name is empty
+		if (right.getName().equals("") || leafName.equals("")) {
+			child = copy(right);
+			if (leafName.equals("")) {
+				child.setName(right.getName());
+			}
+			else {
+				child.setName(leafName);
+			}
+			child.getChildren().addAll(right.getChildren());
 		}
 		else {
+			child = new ViewTree(leafName);
 			child.getChildren().add(right);
 		}
 		left.getChildren().add(child);
 	}
 
-	protected static ViewTree calculateDifference(ViewTree left, ViewTree right) {
-		return new ViewTree("Difference");
+	/**
+	 * Copy Viewtree (ignores children)
+	 */
+	private static ViewTree copy(ViewTree viewTree) {
+		ViewTree copy = new ViewTree(viewTree.getName());
+		copy.setPromise(viewTree.getPromise());
+		copy.setName(viewTree.getName());
+		copy.setFormat(viewTree.getFormat());
+		copy.setIcon(viewTree.getIcon());
+		return copy;
 	}
+
 }
--- org.eclipse.epsilon.picto.diff/src/org/eclipse/epsilon/picto/diff/source/PictoDiffSource.java
+++ org.eclipse.epsilon.picto.diff/src/org/eclipse/epsilon/picto/diff/source/PictoDiffSource.java
@@ -1,28 +1,15 @@
 package org.eclipse.epsilon.picto.diff.source;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.epsilon.common.util.StringProperties;
-import org.eclipse.epsilon.emc.plainxml.PlainXmlModel;
-import org.eclipse.epsilon.eol.EolModule;
-import org.eclipse.epsilon.eol.models.IModel;
-import org.eclipse.epsilon.picto.StringContentPromise;
 import org.eclipse.epsilon.picto.ViewTree;
 import org.eclipse.epsilon.picto.diff.FileWrapperEditorPart;
-import org.eclipse.epsilon.picto.diff.PictoDiffPlugin;
 import org.eclipse.epsilon.picto.diff.ViewTreeMerger;
-import org.eclipse.epsilon.picto.diff.engines.dot.DotDiffContext;
-import org.eclipse.epsilon.picto.diff.engines.dot.DotDiffEngine;
-import org.eclipse.epsilon.picto.diff.engines.dot.DotDiffEngine.DISPLAY_MODE;
 import org.eclipse.epsilon.picto.dom.Parameter;
 import org.eclipse.epsilon.picto.dom.Picto;
 import org.eclipse.epsilon.picto.dom.PictoFactory;
@@ -43,7 +30,7 @@ public class PictoDiffSource extends SimpleSource {
 	public String getFileExtension() {
 		return "pictodiff";
 	}
-	
+
 	@Override
 	public Picto getRenderingMetadata(IEditorPart editorPart) {
 		IFile diffFile = getFile(editorPart);
@@ -52,7 +39,8 @@ public class PictoDiffSource extends SimpleSource {
 			metadata.setTemplate(getFile(editorPart).getLocation().toOSString());
 			metadata.setFormat(getFormat());
 			// get the two internal picto files as parameters of the Picto dom
-			// TODO: check that both files have compatible representations/technologies for diffing
+			// TODO: check that both files have compatible representations/technologies for
+			// diffing
 			try {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(diffFile.getContents(true)));
 				Parameter pLeft = PictoFactory.eINSTANCE.createParameter();
@@ -70,14 +58,15 @@ public class PictoDiffSource extends SimpleSource {
 		}
 		return null;
 	}
-	
+
 	@Override
 	public boolean supports(IEditorPart editorPart) {
-		if (!super.supports(editorPart)) return false;
+		if (!super.supports(editorPart))
+			return false;
 		Picto picto = getRenderingMetadata(editorPart);
 		return picto != null;
 	}
-	
+
 	@Override
 	public ViewTree getViewTree(IEditorPart editor) throws Exception {
 
@@ -88,84 +77,34 @@ public class PictoDiffSource extends SimpleSource {
 
 		IProject project = null;
 		if (editor.getEditorInput() instanceof IFileEditorInput) {
-			IFileEditorInput input = (IFileEditorInput)editor.getEditorInput();
+			IFileEditorInput input = (IFileEditorInput) editor.getEditorInput();
 			project = input.getFile().getProject();
 		}
 		if (project == null) {
-			return null;
+			return createEmptyViewTree();
 		}
-		FileWrapperEditorPart leftWrapper = 
+		FileWrapperEditorPart leftWrapper =
 				new FileWrapperEditorPart(project.getFile(new Path(pLeft.getFile())));
-		FileWrapperEditorPart rightWrapper = 
+		FileWrapperEditorPart rightWrapper =
 				new FileWrapperEditorPart(project.getFile(new Path(pRight.getFile())));
 
 		PictoSource leftPictoSource = getSource(leftWrapper);
 		PictoSource rightPictoSource = getSource(rightWrapper);
 
-		// asuming all we are working with now are dot files
-		DotDiffContext diffContext = 
-				new DotDiffContext(
-						leftWrapper.getFile().getLocation().toOSString(),
-						rightWrapper.getFile().getLocation().toOSString());
-		DotDiffEngine diffEngine = new DotDiffEngine(diffContext, DISPLAY_MODE.CHANGED);
-		diffEngine.load();
-		diffEngine.compare();
+		ViewTree leftViewTree = leftPictoSource.getViewTree(leftWrapper);
+		ViewTree rightViewTree = rightPictoSource.getViewTree(rightWrapper);
+		ViewTree mergedDiffViewTree = ViewTreeMerger.diffMerge(leftViewTree, rightViewTree);
 
-		File tempDir = Files.createTempDirectory("picto").toFile();
-		File temp = Files.createTempFile(tempDir.toPath(), "temp-svg", ".dot").toFile();
-		diffEngine.saveSVGFile(temp);
-		
-		PlainXmlModel xml_model = new PlainXmlModel();
-		StringProperties targetProperties = new StringProperties();
-		targetProperties.put(PlainXmlModel.PROPERTY_FILE, temp.getAbsolutePath());
-		targetProperties.put(PlainXmlModel.PROPERTY_NAME, "M");
-		targetProperties.put(PlainXmlModel.PROPERTY_READONLOAD, "true");
-		targetProperties.put(PlainXmlModel.PROPERTY_STOREONDISPOSAL, "true");
-		xml_model.load(targetProperties);
-
-
-		ArrayList<IModel> allTheModels = new ArrayList<IModel>();
-		allTheModels.add(xml_model);
-		
-		EolModule eolModule = new EolModule();
-		for (IModel theModel : allTheModels) {
-			eolModule.getContext().getModelRepository().addModel(theModel);
-		}
-		java.net.URI eolFile = PictoDiffPlugin.getDefault().getBundle()
-				.getResource("transformations/addScriptToSVG.eol").toURI();
-		eolModule.parse(eolFile);
-		eolModule.execute();
-		eolModule.getContext().getModelRepository().dispose();
-		
-		String c = new String(Files.readAllBytes(Paths.get(temp.toURI())));
-		
 		ViewTree viewTree = new ViewTree();
-		String graph_format = "html";
-		String graph_icon = "diagram-ff0000";
-		ArrayList<String> p = new ArrayList<String>();
-		p.add("Diff Model");
-		viewTree.addPath(p, new StringContentPromise(c), graph_format, graph_icon, new ArrayList<>());
-		
-		p.remove(0);
-		p.add("Left Model");
-		StringContentPromise promise = new StringContentPromise(diffContext.getSourceGraphPromise());
-		viewTree.addPath(p, promise, graph_format, graph_icon, new ArrayList<>());
-		
-		p.remove(0);
-		p.add("Right Model");
-		promise = new StringContentPromise(diffContext.getTargetGraphPromise());
-		viewTree.addPath(p, promise, graph_format, graph_icon, new ArrayList<>());
+		ViewTreeMerger.append(viewTree, mergedDiffViewTree, "Differences");
+		ViewTreeMerger.append(viewTree, leftViewTree, "Original Left");
+		ViewTreeMerger.append(viewTree, rightViewTree, "Original Right");
 
-		p.remove(0);
-		p.add("");
-		ViewTreeMerger.append(viewTree, leftPictoSource.getViewTree(leftWrapper), p, "Original Left");
-		ViewTreeMerger.append(viewTree, rightPictoSource.getViewTree(rightWrapper), p,  "Original Right");
-		
 		return viewTree;
 	}
 
 	protected PictoSource getSource(IEditorPart editorPart) {
-		List<PictoSource> sources = 
+		List<PictoSource> sources =
 				new PictoSourceExtensionPointManager().getExtensions();
 		for (PictoSource source : sources) {
 			if (source.supports(editorPart)) {
